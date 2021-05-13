9bfa1a17882a668b45973a3c3920e210b2c3413a
bkochanowski <kochan88@gmail.com>
2021-05-13 22:09:00 +0200

    task display with horizontal scroll Implemented - 3 random tasks added to test display

--- app/view.py
+++ app/view.py
@@ -1,4 +1,18 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.properties import StringProperty, NumericProperty
+
+from kivy.metrics import sp, dp
+from kivy.utils import rgba
+
+
+class Task(BoxLayout):
+    """this class represent each new task added by the user"""
+    name = StringProperty('')
+    time = StringProperty('')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 
 class MainWindow(BoxLayout):
