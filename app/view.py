3af5fa8b6828d869798c91d4a2eb1524cc5cd07f
bkochanowski <kochan88@gmail.com>
2021-05-19 22:43:25 +0200

    logic for adding new task implemented: - datepicker module added to project - time picker implementation started

--- app/view.py
+++ app/view.py
@@ -1,12 +1,30 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.modalview import ModalView
+from kivy.garden.circulardatetimepicker import CircularTimePicker as CTP
+
 from kivy.properties import StringProperty, NumericProperty
 
 from kivy.metrics import sp, dp
 from kivy.utils import rgba
 
 
-class Task(BoxLayout):
+class NewTask(ModalView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def get_time(self):
+        mv = ModalView(.6, .7)
+        cl = CTP()
+        cl.bind(time=set_time)
+        mv.add_widget(cl)
+        mv.open()
+
+    def set_time(self, inst, value):
+        print(value)
+
+
+class Task(ButtonBehavior, BoxLayout):
     """this class represent each new task added by the user"""
     name = StringProperty('')
     time = StringProperty('')
@@ -16,4 +34,9 @@ class Task(BoxLayout):
 
 
 class MainWindow(BoxLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def add_new(self):
+        nt = NewTask()
+        nt.open()
