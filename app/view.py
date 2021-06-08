24f89444703da5da407ca7e2e1f7907a9a784fd8
bkochanowski <kochan88@gmail.com>
2021-06-09 00:28:21 +0200

    database .py file implemented -> path added to view.py all task logic moved to main root again to fix problem of displaying new widgets

--- app/view.py
+++ app/view.py
@@ -2,14 +2,13 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.button import Button
 from kivy.uix.modalview import ModalView
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.clock import Clock
+
 from kivy.garden.circulardatetimepicker import CircularTimePicker as CTP
 
-from kivy.properties import ObjectProperty, StringProperty, NumericProperty
+from kivy.properties import StringProperty
 
 from kivy.metrics import sp, dp
-from kivy.utils import rgba
+from app.storage.db import Database
 
 
 class NewTask(ModalView):
@@ -25,7 +24,7 @@ class NewTask(ModalView):
         cl.bind(time=self.set_time)
 
         submit = Button(text='Zapisz', background_normal='',
-                        color=[0, .3, 0, .7], background_color=[1, 1, 1, 1], size_hint_y=.2)
+                        color=[0, .3, 0, .7], background_color=[1, 1, 1, 1], size_hint_y=.15)
         submit.bind(on_release=lambda x: self.update_time(cl.time, mv))
         box.add_widget(cl)
         box.add_widget(submit)
@@ -53,30 +52,36 @@ class Task(ButtonBehavior, BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-
 class ItemToBuy(ButtonBehavior, BoxLayout):
-    """this class represent each new task added by the user"""
+    """this class represent each new shopping item added by the user"""
     item_name = StringProperty('')
-    quantity = NumericProperty('')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
 
 class MainWindow(BoxLayout):
-    add_one = ObjectProperty(None)
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.add_one = TaskScreen()
-
-    # def update_widgets(self):
-    #     self.task = str(self.add_one.add_task())
-
-
-class TaskScreen(Screen):
-    def __init__(self, **kwargs):
-        super(TaskScreen, self).__init__(**kwargs)
+    #     self.db = Database()
+    #     self.init.view()
+    #
+    # def init_view(self):
+    #     all_tasks = self.db.get_tasks()
+    #     scroll_parent = self.ids.scroll_parent
+    #     uw = self.ids.upcoming_wrapper
+    #
+    #     for t in all_tasks:
+    #         task = Task()
+    #         task.name = t[0]
+    #         task.details = t[1]
+    #         date, time = t[3].split(' ', 1)
+    #         task.time = time
+    #         task.date = date
+    #         task.size_hint = [None, None]
+    #         task.size = [scroll_parent.width / 1.5, scroll_parent.height -
+    #                      (.050 * scroll_parent.height)]
+    #         uw.add_widget(task)
 
     def add_new(self):
         nt = NewTask()
@@ -91,7 +96,6 @@ class TaskScreen(Screen):
                 t.hint_text = '**Pole wymagane**'
                 t.hint_text_color = [1, 0, 0, 1]
                 error = True
-
         if error:
             print('za krótka informacja o zadaniu')
         else:
@@ -104,9 +108,19 @@ class TaskScreen(Screen):
             task.size = [scroll_parent.width / 1.5, scroll_parent.height -
                          (.050 * scroll_parent.height)]
 
+            date = ''.join([xtask[3].text, xtask[2].text])
+            task_ = (xtask[0].text, xtask[1].text, date)
+            # if self.db.add_task(task_):
             uw.add_widget(task)
             mv.dismiss()
 
+            # if len(uw.children) > 1:
+            #     for child in uw.children:
+            #         if type(child) == NewButton:
+            #             uw.remove_widget(child)
+            #             break
 
-class ShoppingScreen(Screen):
-    pass
+    # def delete_task(self, task: Task):
+    #     name = task.name
+    #     if self.db.delete_task(name):
+    #         task.parent.remove_widget(task)
