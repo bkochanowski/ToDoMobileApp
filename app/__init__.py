d766a71221c2da4249d3753060a4231681364649
bkochanowski <kochan88@gmail.com>
2021-06-12 21:42:28 +0200

    config file created to manage keyboard modes in app

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ from kivy.app import App
 from .view import MainWindow
 from kivy.core.window import Window
 
-#the purpose of this is to simulate smartphone screen size
+# the purpose of this is to simulate smartphone screen size
 Window.size = (320, 545)
 
 
 class MainApp(App):
     title = 'Kivy To-do and Shopping List app'
+
+    def build_config(self, config):
+        config.setdefaults('kivy', {'keyboard_mode': ''})
 
     def build(self):
         return MainWindow()
