e353a1c965e58efafcab55063f851ea642fc5b53
bkochanowski <kochan88@gmail.com>
2021-07-01 22:39:49 +0200

    DB shopping list TABLE updated. Now it stores info if the product was bought or not (is_done)

--- app/storage/db.py
+++ app/storage/db.py
@@ -11,7 +11,8 @@ class Database(object):
 
             sql_tasks = '''CREATE TABLE tasks(id integer primary key, name text not null, details text, 
             date text not null)'''
-            sql_shopping = '''CREATE TABLE shopping(id integer primary key, name text not null)'''
+            sql_shopping = '''CREATE TABLE shopping(id integer primary key, is_done text DEFAULT "False" NOT NULL, 
+            name text not null) '''
 
             cur.execute(sql_tasks)
             cur.execute(sql_shopping)
@@ -59,7 +60,6 @@ class Database(object):
 
         try:
             sql_tasks = '''UPDATE tasks SET name=?, details=?, date=? WHERE name=?'''
-            task.append(task[0])
             cur.execute(sql_tasks, task)
             conn.commit()
 
@@ -99,13 +99,29 @@ class Database(object):
             print(e)
             return False
 
-    def add_item(self, shopping_item:str):
+    def add_item(self, shopping_item: str):
         conn = self.db_connect()
         cur = conn.cursor()
 
         try:
             sql_shopping = 'INSERT INTO shopping (name) VALUES(?)'
             cur.execute(sql_shopping, (shopping_item,))
+            conn.commit()
+
+            return True
+
+        except Exception as e:
+            print(e)
+            return False
+
+    def update_item_status(self, item: list):
+        conn = self.db_connect()
+        cur = conn.cursor()
+
+        try:
+            sql_shopping = '''UPDATE shopping SET is_done=? WHERE name=?'''
+            item.append(item[0])
+            cur.execute(sql_shopping, item)
             conn.commit()
 
             return True
